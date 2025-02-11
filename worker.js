ff790e20fe97b56af63479dc1cce463736394b91
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-02-11 13:58:09 -0800

    Add discord logger integration
    to make sure everything is firing properly.

--- worker.js
+++ worker.js
@@ -1,8 +1,43 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const transportKey = "WORKERID_KEY";
+const Logger = {
+  useForwarder: false,
+  logger: null,
+  serviceName: "",
+
+  configure: function(env) {
+      this.logger = env.LOGGER;
+      this.serviceName = env.WORKER_NAME;
+      this.useForwarder = env.USE_LOG_FORWARDER === "true";
+  },
+  log: async function(msg) {
+      if (!this.useForwarder)
+      {
+          console.log(`${this.serviceName}: ${msg}`);
+          return;
+      }
+      await this.logger.postLog(this.serviceName, msg);
+  },
+  error: async function(msg) {
+      if (!this.useForwarder)
+      {
+          console.error(`${this.serviceName}: ${msg}`);
+          return;
+      }
+      await this.logger.postError(this.serviceName, msg);
+  },
+  warn: async function(msg) {
+        if (!this.useForwarder)
+        {
+            console.warn(`${this.serviceName}: ${msg}`);
+            return;
+        }
+        await this.logger.postWarning(this.serviceName, msg);
+    }
+};
 
 async function ClearGalleryCache(env) {
+  const transportKey = "WORKERID_KEY";
   const helperKey = uuidv4();
   await env.CACHE_KV.put(transportKey, helperKey);
 
@@ -17,9 +52,10 @@ async function ClearGalleryCache(env) {
   const apolloGalleryResponse = await fetch(`https://${env.GALLERY_HOSTNAME}/cf-worker`, init);
   if (apolloGalleryResponse.ok) {
     const jsonBlob = await apolloGalleryResponse.text();
+    await Logger.log(`Updated Apollo Gallery with result ${jsonBlob}`);
     return jsonBlob;
   } else {
-    console.error(apolloGalleryResponse.status);
+    await Logger.error(`Failed to update Apollo Gallery: ${apolloGalleryResponse.status}`);
     return "{'handled': false}";
   }
 }
@@ -28,12 +64,14 @@ export default {
   async fetch(request, env, ctx) {
       const {pathname} = new URL(request.url);
       if (pathname === "/test") {
+        Logger.configure(env);
         const testReturn = await ClearGalleryCache(env);
         return new Response(testReturn, {status: 200, headers: new Headers({"content-type": "application/json"})});
       }
       return new Response('Hello World!');
   },
   async scheduled(event, env, ctx) {
+    Logger.configure(env);
     await ClearGalleryCache(env);
   }
 };
